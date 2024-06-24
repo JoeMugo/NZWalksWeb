61ba3c79b70c4895f5f9e3c4b04f78ed369f3035
Joel Mugo <joelmugo254@gmail.com>
2024-06-24 19:41:18 +0300

    First Commit

---
+++ WalksAPI/Controllers/RegionsController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WalksAPI.Data;
+using WalksAPI.Models.Domain;
+
+namespace WalksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+
+        private readonly NZWalksDbContext dbContext;
+        
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        //GET ALL REGIONS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = dbContext.Regions.ToList();
+
+            return Ok(regions);
+
+
+            //var regions = new List<Region>
+            //{
+            //    new Region
+            //    {
+            //        Id = Guid.NewGuid(),
+            //        Name = "Western Region",
+            //        Code = "WST",
+            //        RegionImageUrl = "https://pin.it/4It3hTjtc"
+            //    },
+
+            //    new Region
+            //    {
+            //        Id = Guid.NewGuid(),
+            //        Name = "Coastal Region",
+            //        Code = "CSTL",
+            //        RegionImageUrl = "https://pin.it/3fjiWQKnX"
+            //    },
+
+            //    new Region
+            //    {
+            //        Id = Guid.NewGuid(),
+            //        Name = "Nairobi Region",
+            //        Code = "NRB",
+            //        RegionImageUrl = "https://pin.it/29ZBRwXz2"
+            //    },
+
+            //};
+
+
+
+        }
+
+        //GET REGION BY ID
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            //var region = dbContext.Regions.FirstOrDefault(x => x.Id ==id); 
+            //OR
+
+            var region = dbContext.Regions.Find(id);
+            if (region == null)
+            {
+                return NotFound();
+            }
+             return Ok(region);
+        }
+    }
+}
---
+++ WalksAPI/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WalksAPI.Controllers
+{
+    // https://localhost:portnumber/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // https://localhost:portnumber/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "Joel", "Hope", "Abby", "Alvin" };
+
+            return Ok(studentNames);
+        }
+    }
+}
